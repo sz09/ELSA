6a85169974abcbb44e96eb107310e12f7f41ef52
Phat Pham Hoang <phatph.se@gmail.com>
2024-10-28 08:01:42 +0700

    fix bug

--- ELSA.Web.Client/src/app/public/question-component/single-choice-question/single-choice-question.component.ts
+++ ELSA.Web.Client/src/app/public/question-component/single-choice-question/single-choice-question.component.ts
@@ -9,15 +9,51 @@ import { optionPerRows } from '../../../../const/const';
   templateUrl: './single-choice-question.component.html',
   styleUrl: './single-choice-question.component.scss'
 })
-export class SingleChoiceQuestionComponent extends QuestionComponentBase<number> {
+export class SingleChoiceQuestionComponent extends QuestionComponentBase<number> implements OnInit {
+  ngOnInit(): void {
+    this.question.options.forEach((d, i) => {
+      this.tempModels[i] = false;
+    });
+  }
+  override collectAnswer() {
+    if(!this.validate()){
+      this.showWarning();
+      return;
+    }
+    this.question.options.forEach((_, i)=> {
+      if(!!this.tempModels[i]){
+        this.answer = i;
+        return;
+      }
+    })
+    this.onCollectedEmitter.emit(this.answer);
+  }
+
+  protected override validate(): boolean {
+    if(Object.values(this.tempModels).filter(d => !!d).length === 0){
+      return false;
+    }
+
+    return true;
+  }
   get batchOptions() {
     return batch(this.question.options ?? [], optionPerRows)
   } 
   @Input()
   question!: SingleChoiceQuestion;
 
+  tempModels: {
+    [index: number]: boolean
+  } = {};
   
   calculateIndex(iBatch: number, iOption: number){
     return iBatch * optionPerRows + iOption;
+  }
+
+  select(index: number) {
+    for(let i = 0; i < this.question.options.length; i ++){
+      this.tempModels[i]  = false;
+    } 
+    this.tempModels[index] = true;
   }
 }
